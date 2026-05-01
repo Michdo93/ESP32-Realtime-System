49732d8b17ea881b54c47b5cd1f5533a723160e7
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2026-05-01 17:31:15 +0200

    Update Constant.py

--- Constant.py
+++ Constant.py
@@ -1,11 +1,10 @@
-
-# 可修改的参数
+# Configurable parameters
 # Reduce displayed waveforms to avoid display freezes
-CSI_VAID_SUBCARRIER_INTERVAL = 1 # interval越大，有效载波数越少
+CSI_VAID_SUBCARRIER_INTERVAL = 1  # Larger interval = fewer valid subcarriers
 
-# 默认参数
+# Default parameters
 # Remove invalid subcarriers
-# secondary channel : below, HT, 40 MHz, non STBC, v, HT-LFT: 0~63, -64~-1, 384
+# Secondary channel: below, HT, 40 MHz, non-STBC, v, HT-LFT: 0~63, -64~-1, 384
 csi_vaid_subcarrier_index = []
 csi_vaid_subcarrier_color = []
 color_step = 255 // (28 // CSI_VAID_SUBCARRIER_INTERVAL + 1)
@@ -16,14 +15,14 @@ csi_vaid_subcarrier_color += [(i * color_step, 0, 0) for i in range(1,  26 // CS
 csi_vaid_subcarrier_index += [i for i in range(33, 59, CSI_VAID_SUBCARRIER_INTERVAL)]    # 26  green
 csi_vaid_subcarrier_color += [(0, i * color_step, 0) for i in range(1,  26 // CSI_VAID_SUBCARRIER_INTERVAL + 2)]
 CSI_DATA_LLFT_COLUMNS = len(csi_vaid_subcarrier_index)
+
 # HT-LFT: 56 + 56
 csi_vaid_subcarrier_index += [i for i in range(66, 94, CSI_VAID_SUBCARRIER_INTERVAL)]    # 28  blue
 csi_vaid_subcarrier_color += [(0, 0, i * color_step) for i in range(1,  28 // CSI_VAID_SUBCARRIER_INTERVAL + 2)]
-csi_vaid_subcarrier_index += [i for i in range(95, 123, CSI_VAID_SUBCARRIER_INTERVAL)]   # 28  White
+csi_vaid_subcarrier_index += [i for i in range(95, 123, CSI_VAID_SUBCARRIER_INTERVAL)]   # 28  white
 csi_vaid_subcarrier_color += [(i * color_step, i * color_step, i * color_step) for i in range(1,  28 // CSI_VAID_SUBCARRIER_INTERVAL + 2)]
-# csi_vaid_subcarrier_index += [i for i in range(124, 162)]  # 28  White
-# csi_vaid_subcarrier_index += [i for i in range(163, 191)]  # 28  White
-
+# csi_vaid_subcarrier_index += [i for i in range(124, 162)]  # 28  white
+# csi_vaid_subcarrier_index += [i for i in range(163, 191)]  # 28  white
 CSI_DATA_COLUMNS = len(csi_vaid_subcarrier_index)
 
 DATA_COLUMNS_NAMES = ["type", "id", "mac", "rssi", "rate", "sig_mode", "mcs", "bandwidth", "smoothing", "not_sounding", "aggregation", "stbc", "fec_coding",
